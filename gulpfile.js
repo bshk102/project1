6d509125c4210b3c08e467c26e2c22c34576c310
Your God <ass@hole>
2021-03-23 12:41:34 +0500

    forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+//const dist = './../OS/OSPanel/domains/project1';
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
---
+++ src/js/modules/forms.js
@@ -0,0 +1,50 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+    phoneInputs.forEach(item => item.addEventListener('input', () => item.value = item.value.replace(/\D/, '')));
+    
+    const message = {
+        loading: 'Загрузка',
+        sucsess: 'Спасибо! Скоро мы с вами сяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => inputs.forEach(item => item.value = '');
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.sucsess;
+                })
+                .catch(() => statusMessage.textContent = message.fail)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => statusMessage.remove(), 4000);
+                });
+        });
+    });
+};
+
+export default forms;
